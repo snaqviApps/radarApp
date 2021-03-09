d8791304c26d377c1c93b9df1c35bc0e537d103e
Sayyid A Naqvi <naqvi.sdsu@gmail.com>
2021-03-09 11:06:51 -0600

    added conflict strategy = REPLACE, app still crashes

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
@@ -1,16 +1,12 @@
 package com.udacity.asteroidradar.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface AsteroidDao {
 
-    @Insert
-//    suspend fun insert(asteroid: Asteroid)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(asteroids: List<Asteroid>)
 
     @Update
